a8526547e643a1049a4d3f008d36ddf9b6ee1894
merlinseela <101634748+merlinseela@users.noreply.github.com>
2024-01-22 19:07:22 +0100

    add automated npc shrooms

--- game/scripts/gameworld/gameworld.gd
+++ game/scripts/gameworld/gameworld.gd
@@ -1,15 +1,44 @@
 extends Node2D
 
+# Tast Manager
+class Task:
+	var task_id # incremented id of task
+	var from_building # path of node
+	var to_building # path of node
+	var resource # which resource to be transported
+	var status # idle, started, done
+
+var task_id = 0
+@export var tasks = []
+
+enum TASKSTATUS {
+	IDLE,
+	INPROCESS,
+	DONE,
+	}
+@export var task_status: TASKSTATUS
+
+
+# Building Manager
+class Building:
+	var building_id # incremented id of building
+	var node_path # will hold the node_name for accessing information of the entity
+	var building # will hold what building it is
+	var location # will hold the global position of the building
+
+var building_id = 0
+@export var buildings = []
+
+# access tilemap
 @onready var tilemap = $TileMap
 
 # preloading all scences
 var mushyScene = preload("res://scenes/units/mushy.tscn")
 var mushyInstance = mushyScene.instantiate()
-
-# preload buildings
+	# preload buildings
 var building_main_scene = preload("res://scenes/buildings/main_building.tscn")
-var building_dirtcleanser_scene = preload("res://scenes/buildings/dirt_cleanser.tscn")
-var building_dirthole_scene = preload("res://scenes/buildings/dirt_hole.tscn")
+var building_dirt_cleanser_scene = preload("res://scenes/buildings/dirt_cleanser.tscn")
+var building_dirt_hole_scene = preload("res://scenes/buildings/dirt_hole.tscn")
 var building_earthworm_farm_scene = preload("res://scenes/buildings/earthworm_farm.tscn")
 var building_house_tent_scene = preload("res://scenes/buildings/house_tent.tscn")
 var building_house_shed_scene = preload("res://scenes/buildings/house_shed.tscn")
@@ -23,22 +52,66 @@ var building_wood_decayer = preload("res://scenes/buildings/wood_decayer.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
-	
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	# check for input if true place
+	# place buildings
 	if (Input.is_action_just_pressed("place_building_main")) == true:
-		add_child(building_main_scene.instantiate())
-	
+		var main_building = building_main_scene.instantiate()
+		add_child(main_building)
+		# create new entry in building list
+		_building_create(main_building.get_path(), "main", main_building.position)
+		
 	if (Input.is_action_just_pressed("place_building_shed")) == true:
 		add_child(building_house_shed_scene.instantiate())
+	if (Input.is_action_just_pressed("place_building_dirt_hole")) == true:
+		add_child(building_dirt_hole_scene.instantiate())
 	
+	# tiles
 	if (Input.is_action_just_pressed("place_tile_street")) == true:
 		var tilePosition = get_node("TileMap").local_to_map(get_viewport().get_mouse_position())
 		tilemap.set_cell(0,tilePosition,0,Vector2i(0,0),0)
 
 	if (Input.is_action_just_pressed("place_tile_street_revert")) == true:
 		pass
-		
+	
+	# deletion
+	if (Input.is_action_just_pressed("delete")) == true:
+		pass # delete keybind not yet set! -> input map
+	
+	
+	# test button
 	if (Input.is_action_just_pressed("test_button")) == true:
-		print(get_node_or_null("/root/Main/Gameworld/MainBuilding").inventory)
+		print(tasks)
+
+
+func _building_create(node_name, building, location):
+	building_id += 1
+	var new_building = Building.new()
+	new_building.node_path = node_name
+	new_building.building = building
+	new_building.location = location
+	buildings.append(new_building)
+
+func _task_create(from_building, to_building, resource):
+	# increment task number by 1
+	task_id += 1
+	var newTask = Task.new()
+	newTask.task_id = task_id
+	newTask.from_building = from_building
+	newTask.to_building = to_building
+	newTask.resource = resource
+	newTask.status = TASKSTATUS.IDLE
+	tasks.append(newTask)
+
+func _task_start():
+	pass
+
+# for now no deletion!
+func _task_done():
+	for task in tasks.size():
+		if tasks[task].status == TASKSTATUS.DONE:
+			print(task)
+
+func _call_test():
+	print("I WAS CALLED: GAMEWORLD")
--- game/scripts/units/npc_shroom_script.gd
+++ game/scripts/units/npc_shroom_script.gd
@@ -1,5 +1,16 @@
 extends CharacterBody2D
 
+@export var inventory = {
+		"water": 0,
+		"dirt": 0,
+		"phosphorus": 0,
+		"potassium": 0,
+		"carbohydrates": 0,
+		"protein": 0,
+		"calcium": 0,
+		"luciferin": 0
+}
+
 var movement_speed: float = 150.0
 var movement_target_position: Vector2
 
@@ -11,7 +22,22 @@ enum STATE{
 	BUSY
 }
 
+enum STATETASK{
+	NOTATFROMBUILDING,
+	EXECUTETASKATFROMBUILDING,
+	NOTATTOBUILDING,
+	EXECUTETASKATTOBUILDING
+}
+
+var state_tracker
+var state_task_tracker
+var loaded_task
+var load_task_success_tracker
+
 func _ready():
+	state_tracker = STATE.IDLE
+	load_task_success_tracker = false
+	
 	# These values need to be adjusted for the actor's speed
 	# and the navigation layout.
 	navigation_agent.path_desired_distance = 4.0
@@ -19,47 +45,68 @@ func _ready():
 
 	# spawn at main building position
 	spawn() # DEV: START AT MAIN
-	#DEV DEBUG
-	# print(get_node("/root/Main/Gameworld/shed").position)
-	
-	
+
 	# Make sure to not await during _ready.
 	call_deferred("actor_setup")
 	
-# Needed later for collison
-	# print(name)
-
 func _process(_delta):
-	if Input.is_action_just_pressed("mouse_right_button"):
-		movement_target_position = get_global_mouse_position()
+	if state_tracker == STATE.IDLE:
+		load_idle_task()
+		if loaded_task != null: 
+			state_tracker = STATE.BUSY
+
+	# configuring the execution of the task
+	if state_tracker == STATE.BUSY and load_task_success_tracker == false:
 		actor_setup()
-	
+		load_task_success_tracker = true
+		state_task_tracker = STATETASK.NOTATFROMBUILDING
+
+	# executing task
+	if state_tracker == STATE.BUSY and load_task_success_tracker == true:
+		match state_task_tracker:
+		# move to NotAtFromBuilding
+			STATETASK.NOTATFROMBUILDING:
+				set_movement_target(get_node(loaded_task.from_building).position)
+				if get_slide_collision_count() > 0:
+					if get_slide_collision(0).get_collider().name == get_node(loaded_task.from_building).name:
+						state_task_tracker = STATETASK.EXECUTETASKATFROMBUILDING
+		# pickup resource
+			STATETASK.EXECUTETASKATFROMBUILDING:
+				# decrese building inventory
+				print("OLD B INV: " + str(get_node(loaded_task.from_building).inventory[loaded_task.resource]))
+				get_node(loaded_task.from_building).inventory[loaded_task.resource] -= 1
+				print("NEW B INV: " + str(get_node(loaded_task.from_building).inventory[loaded_task.resource]))
+				# increase personal inventory
+				print("OLD P INV: " + str(inventory[loaded_task.resource]))
+				inventory[loaded_task.resource] += 1
+				print("NEW P INV: " + str(inventory[loaded_task.resource]))
+				state_task_tracker = STATETASK.NOTATTOBUILDING
+				
+		# walk to to_building
+			STATETASK.NOTATTOBUILDING:
+				set_movement_target(get_node(loaded_task.to_building).position)
+				if get_slide_collision_count() > 0:
+					if get_slide_collision(0).get_collider().name == get_node(loaded_task.to_building).name:
+						state_task_tracker = STATETASK.EXECUTETASKATTOBUILDING
+		
+		# putdown resource
+			STATETASK.EXECUTETASKATTOBUILDING:
+				inventory[loaded_task.resource] -= 1
+				get_node(loaded_task.to_building).inventory[loaded_task.resource] += 1
+				print("MAIN BUILDING INV: " + str(get_node(loaded_task.to_building).inventory))
+				
+				set_task_done()
+				state_tracker = STATE.IDLE
+				load_task_success_tracker = false
+
 func _physics_process(_delta):
-	# decide on next position and move to it
+	# decide on next position and move to it _> DO NOT TOUCH!!!!
 	var current_agent_position: Vector2 = global_position
 	var next_path_position: Vector2 = navigation_agent.get_next_path_position()
 	velocity = current_agent_position.direction_to(next_path_position) * movement_speed
 	move_and_slide()
-
-	# DEV: Walk from A to B 
-	# DEV: localte SHED
-	var shedPosition = get_node("/root/Main/Gameworld/shed").position
-	# assign shed as destination
-	
-	if get_slide_collision_count() > 0:
-		if get_slide_collision(0).get_collider().name == "shed":
-			set_movement_target(get_node("/root/Main/Gameworld/MainBuilding").position)
-		elif get_slide_collision(0).get_collider().name == "MainBuilding":
-			set_movement_target(get_node("/root/Main/Gameworld/shed").position)
-
-	# check if destination is reached, if so end the Agent
 	if navigation_agent.is_navigation_finished():
 		return
-	
-# if collision happens
-	#for i in get_slide_collision_count():
-		#var collision = get_slide_collision(i)
-		#print("Collided with: ", collision.get_collider().name)
 	
 func spawn():
 # detect main building position (grid based) and set mushroom spawn position to the right tile
@@ -76,3 +123,18 @@ func actor_setup():
 
 func set_movement_target(movement_target: Vector2):
 	navigation_agent.target_position = movement_target
+	
+func load_idle_task():
+	for task in get_parent().tasks:
+		if task.status == get_parent().TASKSTATUS.IDLE:
+			loaded_task = task
+			break
+		else:
+			loaded_task = null
+
+func set_task_done():
+	for task in get_parent().tasks:
+		if task.task_id == loaded_task.task_id:
+			task.status = get_parent().TASKSTATUS.DONE
+			get_node(loaded_task.from_building).task_tracker -= 1
+			break
