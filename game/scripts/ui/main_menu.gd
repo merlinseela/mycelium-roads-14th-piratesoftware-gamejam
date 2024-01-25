2390786616a29f937cc48f634ecc52873da57182
SislenC <155790574+SislenC@users.noreply.github.com>
2024-01-25 15:22:05 -0500

    Add into splash

--- game/scripts/ui/main_menu.gd
+++ game/scripts/ui/main_menu.gd
@@ -1,6 +1,7 @@
 extends Control
 
 var title_tween :Tween
+var intro_tween :Tween
 var save_file
 
 #Simple tween to make the title wiggle.
@@ -8,6 +9,14 @@ func _ready():
 	title_tween = create_tween()
 	title_tween.tween_property(%GameTitle, "rotation", .15, 5)
 	title_tween.tween_callback(_title_wiggle)
+	
+	intro_tween = create_tween().set_parallel(true)
+	intro_tween.tween_property(%LandratLogo, "rotation", 0, 5)
+	intro_tween.tween_property(%LandratLogo, "position", Vector2(576,324), 5)
+	intro_tween.chain().tween_property(%LandratLogo, "rotation", 5, 5).set_delay(5.0)
+	intro_tween.parallel().tween_property(%LandratLogo, "position", Vector2(1372,324), 5).set_delay(5.0)
+	intro_tween.chain().tween_property(%BlackOut, "rotation", .35, 3).set_trans(intro_tween.TRANS_ELASTIC).set_ease(intro_tween.EASE_OUT).set_delay(1.0)
+	intro_tween.chain().tween_property(%BlackOut, "position", Vector2(0,1300), 1)
 
 func _title_wiggle():
 	title_tween.stop()
@@ -20,11 +29,12 @@ func _process(_delta):
 
 #Hide/show save slots with sound effects
 func _on_button_new_game_pressed():
-	if %VBoxSlots.visible == true:
-		$AudioBack.play()
-	elif %VBoxSlots.visible == false:
-		$AudioConfirm.play()
-	%VBoxSlots.visible = !%VBoxSlots.visible
+	get_tree().change_scene_to_file("res://main.tscn")
+	#if %VBoxSlots.visible == true:
+		#$AudioBack.play()
+	#elif %VBoxSlots.visible == false:
+		#$AudioConfirm.play()
+	#%VBoxSlots.visible = !%VBoxSlots.visible
 
 #Quits the app
 func _on_button_quit_pressed():
