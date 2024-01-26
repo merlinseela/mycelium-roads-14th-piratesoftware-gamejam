87d6e0a6a2115604a95a29028507440861abbd37
SislenC <155790574+SislenC@users.noreply.github.com>
2024-01-25 22:04:44 -0500

    Story Update

--- game/scenes/story/story_1.gd
+++ game/scenes/story/story_1.gd
@@ -10,8 +10,9 @@ extends Control
 var story_progress = 0
 
 func _process(_delta):
-	if Input.is_action_just_pressed("ui_accept")==true:
+	if Input.is_action_just_pressed("ui_accept")==true and $Buttons.visible == false:
 		story_progress += 1
+		print_debug(story_progress)
 	
 	if story_progress == 0:
 		%Speaker.text = ""
@@ -101,7 +102,7 @@ func _process(_delta):
 	if story_progress == 17:
 		%Speaker.text = ""
 		%Words.text = "You wake. The sun has risen, the birds are chirping, and the garden is peaceful. "
-		$Visual.visible != $Visual.visible
+		$Visual.visible = false
 		
 	if story_progress == 18:
 		%Speaker.text = ""
@@ -110,7 +111,7 @@ func _process(_delta):
 	if story_progress == 19:
 		%Speaker.text = "SAM"
 		%Words.text = "Gyah! A walking mushroom? I thought these only existed in the books Ma keeps showing me… "
-		$Visual.visible != $Visual.visible
+		$Visual.visible = true
 		$Visual.set_frame(18)	
 		
 	if story_progress == 20:
@@ -126,19 +127,16 @@ func _process(_delta):
 	if story_progress == 22:
 		%Speaker.text = "SAM"
 		%Words.text = "You’re a mushroom after all… maybe I’ll call you Mushy!"
-		$Visual.set_frame(18)	
-		
+		$Visual.set_frame(18)
+	
 	if story_progress == 23:
-		%Speaker.text = "SAM"
-		%Words.text = "You’re a mushroom after all… maybe I’ll call you Mushy!"
-		$Visual.set_frame(18)	
-		
-	## There should be a few dialogue options here 
-		
-	if story_progress == 24:
 		%Speaker.text = "???"
-		%Words.text = "You’re a mushroom after all… maybe I’ll call you Mushy!"
-		$Visual.set_frame(18)	
+		%Words.text = ""
+		$Visual.set_frame(18)
+		%LabelChoice1.text = "...(anything but that)..."
+		%LabelChoice2.text = ":D"
+	## There should be a few dialogue options here 
+	
 	#### INSERT DIALOGUE OPTIONS
 	
 	#MUSHROOM: Dialogue options:
@@ -183,78 +181,14 @@ func _process(_delta):
 		%Speaker.text = "SAM" # Change name depending on the dialogue options
 		%Words.text = "I’ll be back later! See you soon!"
 		$Visual.set_frame(18)	
-	
-#Garden. Night. Rain. The scene is black and white. Sam runs out the back of the house, panting. 
-#
-#The player can hear unintelligible frantic dialogue, yelling. 
-#
-#Sam pauses, and shakes their head, as if to say a definitive “no”, before running into the forest with their head down. 
-#
-#Cut to a few frames of Sam running through the forest, the frame growing darker and darker each time, tears beginning to form and trail behind them. 
-#
-#Sam trips and falls to the ground, wincing in pain. A soft blue light emerges from the ground where they had uncovered the ground. 
-#
-#Sam stares at the blue light in wonder, eyes wide, and wipes a couple tears away from their face. 
-#
-#A small blue mushroom resting in the soil, like a baby bird cocooned by moist grass lays there, glowing. 
-#
-#Tenderly, Sam scoops up the mushroom, before it winces and grumbles itself to wake. 
-#
-#The perspective shifts to that of the mushroom, staring into a bewildered Sam’s face. 
-#
-#The camera darkens/shifts a few times, to simulate blinking and looking around. 
-#
-#SAM: Startled. I-I’m sorry… I didn’t mean to wake you. 
-#
-#SAM: Dialogue. I need to get home… I don’t know where I am anymore and it’s so dark out here… It’s so cold… 
-#
-#Sam huddles into a ball after placing you (the mushroom) back on the soil. 
-#
-#A few seconds pass. 
-#
-#The mushroom’s light grows brighter. The scene lights up with the greens of the forest, and blue from the mushroom light. 
-#
-#Sam pauses, before picking the mushroom up again, and cupping it in his hands tenderly, letting the light guide them. 
-#
-#Soon enough, the two are back at the farm, stepping out of the forest. 
-#
-#SAM: You saved me… 
-#
-#Sam rustles around in the garden, before finding a nice bed of leaves for you, and placing you in the center. They step back inside the farmhouse. You close your eyes and go back to sleep, having spent your energy being a fungi flashlight.  
-#
-#SCENE 2
-#You (the mushroom) wake. The sun has risen, the birds are chirping, and the garden is peaceful. 
-#
-#Sam comes out of the farm and sees you wandering about. Startled, he begins moving side to side/shaking erratically. 
-#
-#SAM:  (Whispering to themselves) Gyah! A walking mushroom? I thought these only existed in the books ma keeps showing me… Could it be a pixie? There’s no way… 
-#
-#Sam pauses, and carefully approaches the mushroom. 
-#
-#The mushroom approaches Sam warmly. 
-#
-#SAM: Well, you are awfully cute, so there’s no way you’d be able to hurt me. You’re a mushroom after all… maybe I’ll call you Mushy!
-#
-#MUSHROOM: Dialogue options: 
-#… (anything but that)....
-#:D
-#
-#SAM: Dialogue answers
-#Sorry…. ummmm…. well you do have a pretty round head… and so do I…. What about Roundhead? Or what about Stropharia caerulea?
-#(No answer necessary here)
-#
-#MUSHROOM: Dialogue options: 
-#… well it’s better than “Mushy”…
-#something else?  (Text box)
-#
-#SAM: Great! I’m so happy you’re here! You really helped me last night, and I want to keep you safe here so no hogs get you out there. 
-#
-#You jump around the garden excitedly, glad to be away from the forest predators. 
-#
-#SAM: Let’s get you set up… but I don’t know what you’d need… Can you show me what you need?
-#
-#You point the player towards some beech wood chips and some water. The resources are gathered to a central point. You do a little happy dance when the task is completed. 
-#
-#SAM: Ah! I see! You need wood chips and water to grow! I wonder what else would help you… (distant yell) I’ll be back later! See you soon!
-#
-#You are left to start the game on the resources you have. You have preliminary growth options given. 
+
+
+func _on_button_choice_1_pressed():
+	if story_progress == 23:
+		story_progress = 24
+		$Buttons.visible = false
+
+func _on_button_choice_2_pressed():
+	if story_progress == 23:
+		story_progress = 25
+		$Buttons.visible = false
